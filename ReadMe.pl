223fd11f229f93ed8d25e175e8cc0e49e29f890d
PSSRdt <54758524+PSSRdt@users.noreply.github.com>
2019-09-01 12:08:29 +0800

    Add files via upload

---
+++ ReadMe.pl
@@ -0,0 +1,34 @@
+ Author: Ruizheng Tian
+ Program name: PSSRdt.pl-Polymorphic SSRs digging tool
+
+_______________________________________________________________________________
+
+ Program name: PSSRdt.pl
+ Author:       Ruizheng Tian 
+ Release date: 2019-9-1(version 1.0)
+ Organization: Northwest A&F University
+
+
+
+ _______________________________________________________________________________
+ _______________________________________________________________________________
+
+
+ DESCRIPTION: Program for the screening and research of 
+ 				the information of polymorphic SSR/STR loci
+
+ SYNTAX:   PSSRdt.pl Parameter <FASTA file>
+
+  	  <FASTAfile>    Single file in FASTA format ,and the fasta file was a data collection
+		  from the multiple transcriptome data of same specie .
+
+
+ EXAMPLE: PSSRdt.pl  10  Species1.fasta
+
+ _______________________________________________________________________________
+
+
+
+^^^ DECLARATION ^^^#
+
+ Please check for arguments and input file ,if no destination file. #
